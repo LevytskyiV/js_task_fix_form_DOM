4932fd68ecc7cc534e0143e408954463fa947edf
Vitaliy <vitaliklevytskyi07@gmail.com>
2024-09-10 18:49:54 +0300

    make build

---
+++ index.594265ba.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("form input").forEach(function(e){var t=document.createElement("label");t.className="field-label",t.htmlFor=e.id,t.textContent=e.name[0].toUpperCase()+e.name.slice(1),e.placeholder=t.textContent,e.before(t)});
+//# sourceMappingURL=index.594265ba.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+document.querySelectorAll("form input").forEach((input)=>{
+    const label = document.createElement("label");
+    label.className = "field-label";
+    label.htmlFor = input.id;
+    label.textContent = input.name[0].toUpperCase() + input.name.slice(1);
+    input.placeholder = label.textContent;
+    input.before(label);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
